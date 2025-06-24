93805302c7b816aed6910578210b386f7f649463
Mohibalikhan <khanmohibali1@gmail.com>
2025-06-24 14:00:24 +0500

    Day-02

--- 02_variables.cpp
+++ 02_variables.cpp
@@ -17,8 +17,70 @@ int main()
 
 
 //Program which calculate the area of the rectangle
+// {
+//     float length = 4,width=10;
+//     float result = length*width;
+//     cout<<"The Area of the rectangle is:"<<result<<endl;
+// }
+
+
+//Declare an integer variable named age and assign your age to it.
+
+// {
+// int age=21;
+// cout<<"My age: "<<age;
+// }
+
+// Multiple Variables
+//Declare three variables of type int named x, y, and z. Assign them the values 10, 20, and 30 respectively
+// {
+//     int x=10,y=20,z=30;
+// }
+
+// Variable Swapping
+// Declare two integer variables a and b with values 5 and 10. Write code to swap their values without using a third variable.
+// {
+//  int a=5;
+//  int b=10;
+//  cout<<"Value of A : "<<a<<endl;
+//  cout<<"Value of B: "<<b<<endl;
+
+// a = a + b;  // a becomes 15
+// b = a - b;  // b becomes 5
+// a = a - b;
+
+ 
+//  cout<<"Value of A : "<<a<<endl;
+//  cout<<"Value of B: "<<b<<endl;
+
+// }
+
+// Variable Swapping
+// Declare two integer variables a and b with values 5 and 10. Write code to swap their values using a third variable
+// {
+//  int a=5;
+//  int b=10;
+//  cout<<"Before Swap"<<endl;
+//  cout<<"Value of A : "<<a<<endl;
+//  cout<<"Value of B: "<<b<<endl;
+ 
+//  int c=a;//5
+//  a=b;//10
+//  b=c;//5
+ 
+//  cout<<"After Swap"<<endl;
+//  cout<<"Value of A : "<<a<<endl;
+//  cout<<"Value of B: "<<b<<endl;
+// }
+
+// Declare two float variables num1 and num2 with values 3.14 and 2.71.
+//  Compute and print their sum, difference, product, and quotient.
 {
-    float length = 4,width=10;
-    float result = length*width;
-    cout<<"The Area of the rectangle is:"<<result<<endl;
+    int num1=3.14;
+    int num2=2.71;
+    cout<<"Sum is: "<<num1+num2 <<"\n";
+    cout<<"Diffrence is: "<<num1-num2<<"\n";
+    cout<<"Product is: "<<num1*num2<<"\n";
+    cout<<"Quotient is: "<<num1/num2<<"\n";
+
 }
---
+++ 04_strings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+
+//String Concatenation
+// {
+//     string fname="Mohib Ali";
+//     string lname="Khan";
+//     string fullname=fname+" "+lname;
+//     cout<<fullname;
+//     return 0;
+
+// }
+
+//String Length
+{
+    string name="MohibAli";
+    cout<<"Length:"<<name.length();
+    return 0;
+}
