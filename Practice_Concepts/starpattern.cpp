041fbbe655c176540eb3e0306d75ce6bb598047e
Mohibalikhan <khanmohibali1@gmail.com>
2025-07-16 14:39:45 +0500

    Day-09

---
+++ Practice_Concepts/starpattern.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+
+//  *
+//  * *
+//  * * *
+//  * * * *
+
+// {
+// 	int num=1;
+// 	for(int i=1; i<=4; i++) //4 rows han mere pas 
+//     {  
+// 		for(int k=1; k<=i; k++) // 10 rows print kardo
+//         {
+// 			cout<<"* ";
+			
+// 		}
+// 		cout << endl;
+// 	}
+// }
+
+
+
+
+//       * 
+//     * *
+//   * * *
+// * * * *
+
+// {
+// 	int num=1;
+// 	for(int i=1; i<=4; i++) //4 rows han mere pas 
+//     {  
+// 		for(int j=3; j>=i; j--)  //star ya space 4 se print karke kam karte jao
+//         {
+// 			cout <<"  ";
+// 		}
+// 		for(int k=1; k<=i; k++) // 10 rows print kardo
+//         {
+// 			cout<<"* ";
+			
+// 		}
+// 		cout << endl;
+// 	}
+// }
+
+
+
+
+//    * 
+//   * *
+//  * * *
+// * * * *
+
+// {
+// 	int num=1;
+// 	for(int i=1; i<=4; i++) //4 rows han mere pas 
+//     {  
+// 		for(int j=3; j>=i; j--)  //star ya space 10 se print karke kam karte jao
+//         {
+// 			cout <<" ";
+// 		}
+// 		for(int k=1; k<=i; k++) // 10 rows print kardo
+//         {
+// 			cout<<"* ";
+			
+// 		}
+// 		cout << endl;
+// 	}
+// }
+
+
+
