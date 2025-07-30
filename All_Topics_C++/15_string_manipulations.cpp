160e7f34214ee4465f4e1b118da2cafdaefdabda
Mohib Ali Khan <khanmohibali1@gmail.com>
2025-07-30 10:20:46 -0700

    Day-14

---
+++ All_Topics_C++/15_string_manipulations.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+// Clear function clears all string
+// {
+//     string myname = "Mohib";
+//     cout<<"Before clear: "<<myname;
+//     myname.clear();
+//     cout<<"\nAfter clear: "<<myname;
+// }
+
+
+
+// Erase function remove specific part or overall clear the string karne  
+// {
+//     string myname = "Mohib";
+//     cout<<"Before clear: "<<myname;
+//     myname.erase(0,2); //0 se remove karna shoro karo or 1 tak karo
+//     cout<<"\nAfter clear: "<<myname;
+// }
+
+
+
+// Replace function with string
+// {
+//     string subject = "C++ Programming";
+//     cout<<"Before clear: "<<subject;
+//     subject.replace(0,3,"Python"); //0 se remove karna shoro karo 2 tak or Python likh do iski jaga
+//     cout<<"\nAfter clear: "<<subject;
+// }
+
+
+
+//Replace function with string variable
+// {
+//     string dit = "Diploma in IT";
+//     string cit = "Certicate";
+//     cout<<"Before clear: "<<dit;
+//     dit.replace(0,7,cit); //0 se start karo 7 remove karo or uski jaga ko cit wale variable se replace kardo
+//     cout<<"\nAfter clear: "<<dit;
+// }
+
+
+
+//Replace function with string variable specific start portion add
+// {
+
+//     string dit = "Diploma in IT";
+//     string cit = "Certificate";     //Certificate
+//     cout<<"Before clear: "<<dit;    //012345678910
+//     dit.replace(0,7,cit,0,7); //0 se start karo 7 tak remove karo or usko cit variable se replace kardo or cit ka Cert tak ka part print karo bs
+//     cout<<"\nAfter clear: "<<dit;
+// }
+
+
+
+// //Replace function with string variable specific portion add
+// {
+
+//     string dit = "Diploma in IT";   //          0 1 2 3 4
+//     string cit = "Certificate";     //C e r t i f i c a t e
+//     cout<<"Before clear: "<<dit;    //0 1 2 3 4 5 6 7 8 9 10
+//     dit.replace(0,7,cit,5,4); //0 se 7 tak remove or cit variable se replace kardo or cit ka fica print karo bs
+//     cout<<"\nAfter clear: "<<dit;
+// }
+
+
+
+//Replace function with string variable with specific character
+// {
+
+//     string dit = "Diploma in IT";    
+//     cout<<"Before clear: "<<dit;    
+//     dit.replace(0,7,7,'$'); //0 se 7 tak remove or 7 dafa $ print kardo
+//     cout<<"\nAfter clear: "<<dit;
+// }
+
+
